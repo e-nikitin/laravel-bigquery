f6771c523ddac4487ecd43b903181bd83f2721a9
Evgenii Nikitin <mail.nikitin@mail.ru>
2019-04-19 00:44:14 +0300

    up wait time in runQuery

--- src/BigQuery.php
+++ src/BigQuery.php
@@ -106,7 +106,7 @@ class BigQuery
         }catch (\Exception $e){
             if ($try <= 0 || $e->getCode() != 403)
                 throw $e;
-            sleep(config('bigquery.sleep_time_403', 5));
+            sleep(config('bigquery.sleep_time_403', 10));
             return $this->runQuery($query, $client, --$try);
         }
     }
